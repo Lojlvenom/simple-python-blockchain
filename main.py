ca02bed1b914b2d572aaea06dcab04a0d5e74d2a
joao camilo <jlf.camilo@icloud.com>
2022-02-12 05:39:28 -0400

    simple blockchain example

---
+++ main.py
@@ -0,0 +1,25 @@
+import fastapi as _fastapi
+import blockchain as _blockchain
+
+bc = _blockchain.Blockchain()
+app = _fastapi.FastAPI()
+
+
+def validade_blockchain():
+    if not bc._is_chain_valid():
+        return _fastapi.HTTPException(
+            status_code= 400, detail="Blockchain nao e valida"
+        )
+    
+# EP PARA ADICIONAR UM BLOCO
+@app.post("/mine_block/")
+def mine_block(data: dict):
+    validade_blockchain()
+    block = bc.mine_block(data)
+    return block
+
+@app.get("/blockchain/")
+def get_blockchain():
+    validade_blockchain
+    chain = bc.chain
+    return chain
